9ae539968b4a2fcb0dd370db2210727bddcb7c98
Lukas Sykora <lukas.sykora@vse.cz>
2024-07-08 15:50:11 +0200

    cupy sparse matrix - fix

--- src/action_rules/action_rules.py
+++ src/action_rules/action_rules.py
@@ -237,13 +237,10 @@ class ActionRules:
         # Pandas and CuPy
         elif self.is_gpu_np and not self.is_gpu_pd:
             if use_sparse_matrix:
-                # from scipy.sparse import csr_matrix as scipy_csr_matrix
-
-                # scipy_matrix = scipy_csr_matrix(df.values)
+                from scipy.sparse import csr_matrix as scipy_csr_matrix
                 from cupyx.scipy.sparse import csr_matrix
-
-                # data = csc_matrix(scipy_matrix, dtype=float).T
-                data = csr_matrix(df.values, dtype=self.np.uint8).T
+                scipy_matrix = scipy_csr_matrix(df.values)
+                data = csc_matrix(scipy_matrix, dtype=float).T
             else:
                 data = self.np.asarray(df.values, dtype=self.np.uint8).T  # type: ignore
         # cuDF and Numpy
