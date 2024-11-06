a19dbf0491eedd32487239caa79b126f3c83ca8c
lukas <lukas.sykora@ogilvy.com>
2024-11-06 16:38:01 +0100

    feat: profiling

--- notebooks/gpu_sparse_vs_dense/log_gpu.sh
+++ notebooks/gpu_sparse_vs_dense/log_gpu.sh
@@ -3,6 +3,6 @@ duration=180  # 3 minutes in seconds
 end_time=$(( $(date +%s) + duration ))
 
 while [ $(date +%s) -lt $end_time ]; do
-    gpustat -cp >> gpu_memory_sparse_matrix.log
+    /home/jupyter-xsykl04@vse.cz/.local/bin/gpustat -cp >> $1
     sleep 1
 done
